8ace322cf4a9d2e77f093fcc7862e6160dee4f30
OlenaYermak <145419464+OlenaYermak@users.noreply.github.com>
2024-09-18 18:54:18 +0300

    pages structure

---
+++ src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,14 @@
+export default function HeroSection() {
+    return (
+        <section>
+            <div>
+                <h1>Make Life Easier for the Family:</h1> 
+                <p>Find Babysitters Online for All Occasions</p>
+                <a>Get started</a>
+            </div>
+            <div>
+
+            </div> 
+        </section>
+    );
+};
