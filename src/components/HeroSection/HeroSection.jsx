7814efae048b22b9ac1f9e2116969ef7f5711129
OlenaYermak <145419464+OlenaYermak@users.noreply.github.com>
2024-10-07 21:25:41 +0300

    hero section
    added hero section styles

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,14 +1,35 @@
-export default function HeroSection() {
-    return (
-        <section>
-            <div>
-                <h1>Make Life Easier for the Family:</h1> 
-                <p>Find Babysitters Online for All Occasions</p>
-                <a>Get started</a>
-            </div>
-            <div>
+import { Link } from 'react-router-dom';
 
-            </div> 
-        </section>
-    );
-};
+import { MdOutlineArrowOutward } from 'react-icons/md';
+import { FaCheck } from 'react-icons/fa';
+
+import css from './HeroSection.module.css';
+
+export default function HeroSection() {
+  return (
+    <section className={css.heroSection}>
+      <div className={css.heroSectionTextWrapper}>
+        <h1 className={css.heroSectionTitle}>
+          Make Life Easier for the Family:
+        </h1>
+        <p className={css.heroSectionText}>
+          Find Babysitters Online for All Occasions
+        </p>
+        <Link to="/nannies" className={css.heroSectionLink}>
+          Get started <MdOutlineArrowOutward className={css.icon} size={18} />
+        </Link>
+      </div>
+      <div className={css.heroSectionPicture}>
+        <div className={css.infoBox}>
+          <div className={css.checkIconWrapper}>
+            <FaCheck size={20} className={css.checkIcon} />
+          </div>
+          <div className={css.textWrapper}>
+            <p className={css.mainText}>Experienced nannies</p>
+            <p className={css.subText}>15,000</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
